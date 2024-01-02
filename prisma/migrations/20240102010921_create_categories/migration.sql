e5c6c2b6de67d2f2b9ccc9ef4f58a0e9f0668c6f
yuseonkim <yuseonkim@icloud.com>
2024-01-02 17:17:33 +0900

    feat : Prisma ORM 연동 및 User, Content 데이터 마이그레이션
     * Prisma ORM 연동
     * User 및 Content 테이블 작성
     * 로컬 mysql DB 연동

---
+++ prisma/migrations/20240102010921_create_categories/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nickname` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_nickname_key`(`nickname`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `content` VARCHAR(191) NOT NULL,
+    `authorId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
